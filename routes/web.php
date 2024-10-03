60d68f8dd3f3fa06823e4c607e1e910607c0e157
fifiinvtsr <149783209+fifiinvtsr@users.noreply.github.com>
2024-10-03 12:43:03 +0700

    Jobsheet 6 Praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,8 @@ Route::group(['prefix' => 'user'], function() {
     Route::post('/list', [UserController::class, 'list']); // Menampilkan data user dalam bentuk json untuk datatables
     Route::get('/create', [UserController::class, 'create']); // Menampilkan halaman form tambah user
     Route::post('/', [UserController::class, 'store']); // Menyimpan data user baru
+    Route::get('/create_ajax', [UserController::class, 'create_ajax']); //Menampilkan halaman form tambah user Ajax
+    Route::post('/ajax', [UserController::class, 'store_ajax']); // Menyimpan data user baru Ajax
     Route::get('/{id}', [UserController::class, 'show']); // Menampilkan detail user
     Route::get('/{id}/edit', [UserController::class, 'edit']); // Menampilkan halaman form user
     Route::put('/{id}', [UserController::class, 'update']); // Menyimpan perubahan data user
