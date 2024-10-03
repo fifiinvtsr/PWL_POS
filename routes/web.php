3e02bb1896352e6f6a8bc930075d8ef6495ef29c
fifiinvtsr <149783209+fifiinvtsr@users.noreply.github.com>
2024-10-03 20:29:53 +0700

    Jobsheet 6 Praktikum 2 & 3

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,10 @@ Route::group(['prefix' => 'user'], function() {
     Route::get('/create_ajax', [UserController::class, 'create_ajax']); //Menampilkan halaman form tambah user Ajax
     Route::post('/ajax', [UserController::class, 'store_ajax']); // Menyimpan data user baru Ajax
     Route::get('/{id}', [UserController::class, 'show']); // Menampilkan detail user
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']); //Menampilkan halaman form edit user Ajax
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']); //Menyimpan perubahan data user Ajax
+    Route::get('{id}/delete_ajax', [UserController::class, 'confirm_ajax']); // Untuk tampilkan form confirm delete user Ajax
+    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']); //Untuk hapus data user Ajax
     Route::get('/{id}/edit', [UserController::class, 'edit']); // Menampilkan halaman form user
     Route::put('/{id}', [UserController::class, 'update']); // Menyimpan perubahan data user
     Route::delete('/{id}', [UserController::class, 'destroy']); // Menghapus data user
