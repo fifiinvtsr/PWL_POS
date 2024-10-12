03e2c63c1d2397ebf8f09f344b975281609de88d
fifiinvtsr <149783209+fifiinvtsr@users.noreply.github.com>
2024-10-12 13:10:52 +0700

    Jobsheet 7 Praktikum & Tugas 3

--- app/Http/Middleware/AuthorizeUser.php
+++ app/Http/Middleware/AuthorizeUser.php
@@ -13,14 +13,18 @@ class AuthorizeUser
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, ... $roles): Response
     {
-        return $next($request);         //ambil data user yang login
-                                        //fungsi user() diambil dari Usermodel.php
-        if($user->$hasRole($role)){     //cek apakah user punya role yang diinginkan
-            return $next($request);
+        // return $next($request);         //ambil data user yang login
+        //                                 //fungsi user() diambil dari Usermodel.php
+        // if($user->$hasRole($role)){     //cek apakah user punya role yang diinginkan
+
+        //Praktiku 3 JS7
+        $user_role = $request->user()->getRole(); //ambil data level_kode dari user yang login
+        if(in_array($user_role, $roles)){ //cek apakah level_kode user ada didalam array roles
+            return $next($request); //jika ada, maka lanjutkan request
         }
-        // jika tidak punya role, maka ditampilkam error 403
-        abort(403, 'Forbiddan. Kamu tidak punya akses ke halaman ini');
-    }
+                // jika tidak punya role, maka ditampilkam error 403
+                abort(403, 'Forbiddan. Kamu tidak punya akses ke halaman ini');
+        }
 }
