ceece99c8c3893cbf3bc4fb90bf8ca7f0891aef4
fifiinvtsr <149783209+fifiinvtsr@users.noreply.github.com>
2024-10-18 13:40:37 +0700

    JS08 Praktikum 3

--- resources/views/barang/index.blade.php
+++ resources/views/barang/index.blade.php
@@ -6,8 +6,12 @@
         <h3 class="card-title">Daftar Barang</h3> 
         <div class="card-tools">
             <button onclick="modalAction('{{ url('/barang/import') }}')" class="btn btn-sm btn-info mt-1">Impor Barang</button>
+            {{-- JS08 Praktikum 2 --}}
             <a href="{{ url('/barang/export_excel') }}" class="btn btn-sm btn-info mt-1"><i class="fa fa-file-excel"></i> Export Barang</a>
+            {{-- JS08 Praktikum 3 --}}
+            <a href="{{ url('/barang/export_pdf') }}" class="btn btn-warning"><i class="fa fa-filepdf"></i> Export Barang</a>
             <button onclick="modalAction('{{url('barang/create_ajax')}}')" class="btn btn-sm btn-success mt-1">Tambah Data (Ajax)</button>
+
         </div>          
       </div> 
       <div class="card-body">
