2f50ee5582536b16caf0212dcfe465ca32314c7d
fifiinvtsr <149783209+fifiinvtsr@users.noreply.github.com>
2024-10-20 11:20:38 +0700

    JS08 Tugas 4

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -425,6 +425,7 @@ class BarangController extends Controller
         $writer->save('php://output');
         exit;
     } // end function export_excel
+    
     public function export_pdf()
     {
         $barang = BarangModel::select('kategori_id', 'barang_kode', 'barang_nama', 'harga_beli', 'harga_jual')
