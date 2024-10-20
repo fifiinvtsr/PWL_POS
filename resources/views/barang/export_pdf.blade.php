b90c6860f3311ef744abc4fccdd97c34e224d65f
fifiinvtsr <149783209+fifiinvtsr@users.noreply.github.com>
2024-10-20 11:19:25 +0700

    JS08 Tugas 4

--- resources/views/barang/export_pdf.blade.php
+++ resources/views/barang/export_pdf.blade.php
@@ -50,7 +50,9 @@
         .border-bottom-header {
             border-bottom: 1px solid;
         }
-        .border-all, .border-all th, .border-all td {
+        .border-all,
+        .border-all th,
+        .border-all td {
             border: 1px solid;
         }
     </style>
@@ -59,18 +61,53 @@
     <table class="border-bottom-header">
         <tr>
             <td width="15%" class="text-center">
-                <img src="{{ asset('polinema-bw.png') }}">
+                <img src="{{ asset('polinema.png') }}">
             </td>
             <td width="85%">
-                <span class="text-center d-block font-11 font-bold mb-1">KEMENTERIAN PENDIDIKAN, KEBUDAYAAN, RISET, DAN TEKNOLOGI</span>
-                <span class="text-center d-block font-13 font-bold mb-1">POLITEKNIK NEGERI MALANG</span>
-                <span class="text-center d-block font-10">Jl. Soekarno-Hatta No. 9 Malang 65141</span>
-                <span class="text-center d-block font-10">Telepon (0341) 404424 Pes. 101-105, 0341-404420, Fax. (0341) 404420</span>
-                <span class="text-center d-block font-10">Laman: www.polinema.ac.id</span>
+                <span class="text-center d-block font-11 font-bold mb-1">
+                    KEMENTERIAN PENDIDIKAN, KEBUDAYAAN, RISET, DAN TEKNOLOGI
+                </span>
+                <span class="text-center d-block font-13 font-bold mb-1">
+                    POLITEKNIK NEGERI MALANG
+                </span>
+                <span class="text-center d-block font-10">
+                    Jl. Soekarno-Hatta No. 9 Malang 65141
+                </span>
+                <span class="text-center d-block font-10">
+                    Telepon (0341) 404424 Pes. 101-105, 0341-404420, Fax. (0341) 404420
+                </span>
+                <span class="text-center d-block font-10">
+                    Laman: www.polinema.ac.id
+                </span>
             </td>
         </tr>
     </table>
 
     <h3 class="text-center">LAPORAN DATA BARANG</h3>
-
-   
+    
+    <table class="border-all">
+        <thead>
+            <tr>
+                <th class="text-center">No</th>
+                <th>Kode Barang</th>
+                <th>Nama Barang</th>
+                <th class="text-right">Harga Beli</th>
+                <th class="text-right">Harga Jual</th>
+                <th>Kategori</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($barang as $b)
+            <tr>
+                <td class="text-center">{{ $loop->iteration }}</td>
+                <td>{{ $b->barang_kode }}</td>
+                <td>{{ $b->barang_nama }}</td>
+                <td class="text-right">{{ number_format($b->harga_beli, 0, ',', '.') }}</td>
+                <td class="text-right">{{ number_format($b->harga_jual, 0, ',', '.') }}</td>
+                <td>{{ $b->kategori->kategori_nama }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</body>
+</html>
--- resources/views/user/create_ajax.blade.php
+++ resources/views/user/create_ajax.blade.php
@@ -1,17 +1,19 @@
-<form action="{{ url('/user/ajax') }}" method="POST" id="form-tambah">
+<form action="{{ url('/user/ajax') }}" method="POST" id="form-tambah" enctype="multipart/form-data">
     @csrf
     <div id="modal-master" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Tambah Data User</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
             <div class="modal-body">
                 <div class="form-group">
                     <label>Level Pengguna</label>
                     <select name="level_id" id="level_id" class="form-control" required>
                         <option value="">- Pilih Level -</option>
-                        @foreach($level as $l)
+                        @foreach ($level as $l)
                             <option value="{{ $l->level_id }}">{{ $l->level_nama }}</option>
                         @endforeach
                     </select>
@@ -28,6 +30,11 @@
                     <small id="error-nama" class="error-text form-text text-danger"></small>
                 </div>
                 <div class="form-group">
+                    <label>Foto Profil</label>
+                    <input type="file" name="file_profil" id="file_profil" class="form-control" required>
+                    <small id="error-file_profil" class="error-text form-text text-danger"></small>
+                </div>
+                <div class="form-group">
                     <label>Password</label>
                     <input value="" type="password" name="password" id="password" class="form-control" required>
                     <small id="error-password" class="error-text form-text text-danger"></small>
@@ -39,54 +46,78 @@
             </div>
         </div>
     </div>
-    </form>
-    <script>
-        $(document).ready(function() {
-            $("#form-tambah").validate({
-                rules: {
-                    level_id: {required: true, number: true},
-                    username: {required: true, minlength: 3, maxlength: 20},
-                    nama: {required: true, minlength: 3, maxlength: 100},
-                    password: {required: true, minlength: 6, maxlength: 20}
+</form>
+
+<script>
+    $(document).ready(function() {
+        $("#form-tambah").validate({
+            rules: {
+                level_id: {
+                    required: true,
+                    number: true
                 },
-                submitHandler: function(form) {
-                    $.ajax({
-                        url: form.action,
-                        type: form.method,
-                        data: $(form).serialize(),
-                        success: function(response) {
-                            if(response.status){
-                                $('#myModal').modal('hide');
-                                Swal.fire({
-                                    icon: 'success',
-                                    title: 'Berhasil',
-                                    text: response.message
-                                });
-                                dataUser.ajax.reload();
-                            }else{
-                                $('.error-text').text('');
-                                $.each(response.msgField, function(prefix, val) {
-                                    $('#error-'+prefix).text(val[0]);
-                                });
-                                Swal.fire({
-                                    icon: 'error',
-                                    title: 'Terjadi Kesalahan',
-                                    text: response.message
-                                });
-                            }
+                username: {
+                    required: true,
+                    minlength: 3,
+                    maxlength: 20
+                },
+                nama: {  // Ganti 'name' menjadi 'nama' agar konsisten dengan controller
+                    required: true,
+                    minlength: 3,
+                    maxlength: 100
+                },
+                password: {
+                    required: true,
+                    minlength: 6,
+                    maxlength: 20
+                },
+                file_profil: {
+                    required: true,
+                    extension: "jpg|jpeg|png|ico|bmp"
+                }
+            },
+            submitHandler: function(form) {
+                var formData = new FormData(form); // Jadikan form ke FormData untuk menghandle file 
+
+                $.ajax({
+                    url: form.action,
+                    type: form.method,
+                    data: formData,
+                    processData: false, // Menjaga agar file dikirim sebagai multipart/form-data
+                    contentType: false,
+                    success: function(response) {
+                        if (response.status) {
+                            $('#myModal').modal('hide');
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Berhasil',
+                                text: response.message
+                            });
+                            dataUser.ajax.reload();
+                        } else {
+                            $('.error-text').text('');
+                            $.each(response.msgField, function(prefix, val) {
+                                $('#error-' + prefix).text(val[0]);
+                            });
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Terjadi Kesalahan',
+                                text: response.message
+                            });
                         }
-                    });
+                    }
+                });
                 return false;
             },
             errorElement: 'span',
-            errorPlacement: function (error, element) {
+            errorPlacement: function(error, element) {
                 error.addClass('invalid-feedback');
                 element.closest('.form-group').append(error);
             },
-            highlight: function (element, errorClass, validClass) {
+            highlight: function(element, errorClass, validClass) {
                 $(element).addClass('is-invalid');
             },
-            unhighlight: function (element, errorClass, validClass) {
+            unhighlight: function(element, errorClass, validClass) {
                 $(element).removeClass('is-invalid');
             }
         });
