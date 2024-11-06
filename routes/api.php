71208eea9b7e9334dcb19295467082afbc8070e4
fifiinvtsr <149783209+fifiinvtsr@users.noreply.github.com>
2024-11-06 13:19:02 +0700

    JS 10 Praktikum 2-4 Login-Logout-Level

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +18,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// praktikum 1 js 9
+// praktikum 1 js 10
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+// praktikum 2 js 10
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//praktikum 3 js 10
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+//praktikum 4 js 10
+// level
+Route::get('level', [LevelController::class, 'index']);
+Route::post('levels', [LevelController::class, 'store']);
+Route::get('levels/{level}', [LevelController::class, 'show']);
+Route::put('levels/{level}', [LevelController::class, 'update']);
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);
